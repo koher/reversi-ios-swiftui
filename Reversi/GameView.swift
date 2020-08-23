57b36a5e7074f007cd7adc9f3765eb8bb1c37db7
Yuta Koshizawa <koher@koherent.org>
2020-08-24 02:45:52 +0900

    Implement inputs for `GameView`

--- Reversi/GameView.swift
+++ Reversi/GameView.swift
@@ -5,50 +5,67 @@ import ReversiLogics
 struct GameView: View {
     @Binding var presenter: GamePresenter
     
-    // FIXME: Replace them with `presenter`'s properties
-    @State var darkPlayer: Player = .manual
-    @State var lightPlayer: Player = .manual
-
     init(presenter: Binding<GamePresenter>) {
         self._presenter = presenter
     }
     
     var body: some View {
-        ZStack {
-            VStack(spacing: 20) {
-                MessageView(presenter.message)
-                Spacer()
-                HStack(spacing: 16) {
-                    DiskView(.light)
-                        .frame(width: 26, height: 26)
-                    PlayerPicker(selection: $lightPlayer)
-                    Text(presenter.count(of: .light).description)
-                        .font(.system(size: 24))
-                    if presenter.isPlayerActivityIndicatorVisible(of: .light) {
-                        ProgressView()
-                    }
-                    Spacer()
-                }
-                BoardView(presenter.manager.game.board) { x, y in
-                    // TODO
-                }
-                HStack(spacing: 16) {
-                    Spacer()
-                    if presenter.isPlayerActivityIndicatorVisible(of: .dark) {
-                        ProgressView()
-                    }
-                    Text(presenter.count(of: .dark).description)
-                        .font(.system(size: 24))
-                    PlayerPicker(selection: $darkPlayer)
-                    DiskView(.dark)
-                        .frame(width: 26, height: 26)
+        VStack(spacing: 20) {
+            MessageView(presenter.message)
+            Spacer()
+            HStack(spacing: 16) {
+                DiskView(.light)
+                    .frame(width: 26, height: 26)
+                PlayerPicker(selection: $presenter.lightPlayer)
+                    .frame(width: 161)
+                Text(presenter.count(of: .light).description)
+                    .font(.system(size: 24))
+                if presenter.isPlayerActivityIndicatorVisible(of: .light) {
+                    ProgressView()
                 }
                 Spacer()
-                Button("Reset") {
-                    // TODO
-                }
             }
-                .padding(20)
+            BoardView(presenter.manager.game.board) { x, y in
+                presenter.tryPlacingDiskAt(x: x, y: y)
+            }
+            HStack(spacing: 16) {
+                Spacer()
+                if presenter.isPlayerActivityIndicatorVisible(of: .dark) {
+                    ProgressView()
+                }
+                Text(presenter.count(of: .dark).description)
+                    .font(.system(size: 24))
+                PlayerPicker(selection: $presenter.darkPlayer)
+                    .frame(width: 161)
+                DiskView(.dark)
+                    .frame(width: 26, height: 26)
+            }
+            Spacer()
+            Button("Reset") {
+                presenter.confirmToReset()
+            }
+            .alert(isPresented: .constant(presenter.isResetAlertVisible)) {
+                Alert(
+                    title: Text("Confirmation"),
+                    message: Text("Do you really want to reset the game?"),
+                    primaryButton: .cancel(Text("Cancel")) {
+                        presenter.completeConfirmationForReset(false)
+                    },
+                    secondaryButton: .destructive(Text("OK")) {
+                        presenter.completeConfirmationForReset(true)
+                    }
+                )
+            }
+        }
+        .padding(20)
+        .alert(isPresented: .constant(presenter.isPassingAlertVisible)) {
+            Alert(
+                title: Text("Pass"),
+                message: Text("Cannot place a disk."),
+                dismissButton: .default(Text("Dismiss")) {
+                    presenter.completeConfirmationForPass()
+                }
+            )
         }
     }
 }
