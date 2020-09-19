a05751d4ee133be65b6b0713a50b42a9547adfd1
Yuta Koshizawa <koher@koherent.org>
2020-09-19 15:19:44 +0900

    Add UIKit views
    Those views are provided by the original project.

---
+++ Reversi/UIKit/_BoardView.swift
@@ -0,0 +1,150 @@
+import UIKit
+import SwiftyReversi
+
+private let lineWidth: CGFloat = 2
+
+public class __BoardView: UIView {
+    private var cellViews: [_CellView] = []
+    private var actions: [CellSelectionAction] = []
+    
+    public let width: Int = 8
+    public let height: Int = 8
+    
+    public let xRange: Range<Int>
+    public let yRange: Range<Int>
+    
+    public weak var delegate: _BoardViewDelegate?
+    
+    override public init(frame: CGRect) {
+        xRange = 0 ..< width
+        yRange = 0 ..< height
+        super.init(frame: frame)
+        setUp()
+    }
+    
+    required public init?(coder: NSCoder) {
+        xRange = 0 ..< width
+        yRange = 0 ..< height
+        super.init(coder: coder)
+        setUp()
+    }
+    
+    private func setUp() {
+        self.backgroundColor = UIColor(named: "DarkColor")!
+        
+        let cellViews: [_CellView] = (0 ..< (width * height)).map { _ in
+            let cellView = _CellView()
+            cellView.translatesAutoresizingMaskIntoConstraints = false
+            return cellView
+        }
+        self.cellViews = cellViews
+        
+        cellViews.forEach(self.addSubview(_:))
+        for i in cellViews.indices.dropFirst() {
+            NSLayoutConstraint.activate([
+                cellViews[0].widthAnchor.constraint(equalTo: cellViews[i].widthAnchor),
+                cellViews[0].heightAnchor.constraint(equalTo: cellViews[i].heightAnchor),
+            ])
+        }
+        
+        NSLayoutConstraint.activate([
+            cellViews[0].widthAnchor.constraint(equalTo: cellViews[0].heightAnchor),
+        ])
+        
+        for y in yRange {
+            for x in xRange {
+                let topNeighborAnchor: NSLayoutYAxisAnchor
+                if let cellView = cellViewAt(x: x, y: y - 1) {
+                    topNeighborAnchor = cellView.bottomAnchor
+                } else {
+                    topNeighborAnchor = self.topAnchor
+                }
+                
+                let leftNeighborAnchor: NSLayoutXAxisAnchor
+                if let cellView = cellViewAt(x: x - 1, y: y) {
+                    leftNeighborAnchor = cellView.rightAnchor
+                } else {
+                    leftNeighborAnchor = self.leftAnchor
+                }
+                
+                let cellView = cellViewAt(x: x, y: y)!
+                NSLayoutConstraint.activate([
+                    cellView.topAnchor.constraint(equalTo: topNeighborAnchor, constant: lineWidth),
+                    cellView.leftAnchor.constraint(equalTo: leftNeighborAnchor, constant: lineWidth),
+                ])
+                
+                if y == height - 1 {
+                    NSLayoutConstraint.activate([
+                        self.bottomAnchor.constraint(equalTo: cellView.bottomAnchor, constant: lineWidth),
+                    ])
+                }
+                if x == width - 1 {
+                    NSLayoutConstraint.activate([
+                        self.rightAnchor.constraint(equalTo: cellView.rightAnchor, constant: lineWidth),
+                    ])
+                }
+            }
+        }
+        
+        reset()
+        
+        for y in yRange {
+            for x in xRange {
+                let cellView: _CellView = cellViewAt(x: x, y: y)!
+                let action = CellSelectionAction(boardView: self, x: x, y: y)
+                actions.append(action) // To retain the `action`
+                cellView.addTarget(action, action: #selector(action.selectCell), for: .touchUpInside)
+            }
+        }
+    }
+    
+    public func reset() {
+        for y in  yRange {
+            for x in xRange {
+                setDisk(nil, atX: x, y: y, animated: false)
+            }
+        }
+        
+        setDisk(.light, atX: width / 2 - 1, y: height / 2 - 1, animated: false)
+        setDisk(.dark, atX: width / 2, y: height / 2 - 1, animated: false)
+        setDisk(.dark, atX: width / 2 - 1, y: height / 2, animated: false)
+        setDisk(.light, atX: width / 2, y: height / 2, animated: false)
+    }
+    
+    private func cellViewAt(x: Int, y: Int) -> _CellView? {
+        guard xRange.contains(x) && yRange.contains(y) else { return nil }
+        return cellViews[y * width + x]
+    }
+    
+    public func diskAt(x: Int, y: Int) -> Disk? {
+        cellViewAt(x: x, y: y)?.disk
+    }
+    
+    public func setDisk(_ disk: Disk?, atX x: Int, y: Int, animated: Bool, completion: ((Bool) -> Void)? = nil) {
+        guard let cellView = cellViewAt(x: x, y: y) else {
+            preconditionFailure() // FIXME: Add a message.
+        }
+        cellView.setDisk(disk, animated: animated, completion: completion)
+    }
+}
+
+public protocol _BoardViewDelegate: AnyObject {
+    func boardView(_ boardView: __BoardView, didSelectCellAtX x: Int, y: Int)
+}
+
+private class CellSelectionAction: NSObject {
+    private weak var boardView: __BoardView?
+    let x: Int
+    let y: Int
+    
+    init(boardView: __BoardView, x: Int, y: Int) {
+        self.boardView = boardView
+        self.x = x
+        self.y = y
+    }
+    
+    @objc func selectCell() {
+        guard let boardView = boardView else { return }
+        boardView.delegate?.boardView(boardView, didSelectCellAtX: x, y: y)
+    }
+}
---
+++ Reversi/UIKit/_CellView.swift
@@ -0,0 +1,140 @@
+import UIKit
+import SwiftyReversi
+
+private let animationDuration: TimeInterval = 0.25
+
+public class _CellView: UIView {
+    private let button: UIButton = UIButton()
+    private let diskView: _DiskView = _DiskView()
+    
+    private var _disk: Disk?
+    public var disk: Disk? {
+        get { _disk }
+        set { setDisk(newValue, animated: true) }
+    }
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+    
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUp()
+    }
+    
+    private func setUp() {
+        do { // button
+            button.translatesAutoresizingMaskIntoConstraints = false
+            do { // backgroundImage
+                UIGraphicsBeginImageContext(CGSize(width: 1, height: 1))
+                defer { UIGraphicsEndImageContext() }
+                
+                let color: UIColor = UIColor(named: "CellColor")!
+                color.set()
+                UIRectFill(CGRect(x: 0, y: 0, width: 1, height: 1))
+                
+                let backgroundImage = UIGraphicsGetImageFromCurrentImageContext()!
+                button.setBackgroundImage(backgroundImage, for: .normal)
+                button.setBackgroundImage(backgroundImage, for: .disabled)
+            }
+            self.addSubview(button)
+        }
+
+        do { // diskView
+            diskView.translatesAutoresizingMaskIntoConstraints = false
+            self.addSubview(diskView)
+        }
+
+        setNeedsLayout()
+    }
+    
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        button.frame = bounds
+        layoutDiskView()
+    }
+    
+    private func layoutDiskView() {
+        let cellSize = bounds.size
+        let diskDiameter = Swift.min(cellSize.width, cellSize.height) * 0.8
+        let diskSize: CGSize
+        if _disk == nil || diskView.disk == _disk {
+            diskSize = CGSize(width: diskDiameter, height: diskDiameter)
+        } else {
+            diskSize = CGSize(width: 0, height: diskDiameter)
+        }
+        diskView.frame = CGRect(
+            origin: CGPoint(x: (cellSize.width - diskSize.width) / 2, y: (cellSize.height - diskSize.height) / 2),
+            size: diskSize
+        )
+        diskView.alpha = _disk == nil ? 0.0 : 1.0
+    }
+    
+    public func setDisk(_ disk: Disk?, animated: Bool, completion: ((Bool) -> Void)? = nil) {
+        let diskBefore: Disk? = _disk
+        _disk = disk
+        let diskAfter: Disk? = _disk
+        if animated {
+            switch (diskBefore, diskAfter) {
+            case (.none, .none):
+                completion?(true)
+            case (.none, .some(let animationDisk)):
+                diskView.disk = animationDisk
+                fallthrough
+            case (.some, .none):
+                UIView.animate(withDuration: animationDuration, delay: 0, options: .curveEaseIn, animations: { [weak self] in
+                    self?.layoutDiskView()
+                }, completion: { finished in
+                    completion?(finished)
+                })
+            case (.some, .some):
+                UIView.animate(withDuration: animationDuration / 2, delay: 0, options: .curveEaseOut, animations: { [weak self] in
+                    self?.layoutDiskView()
+                }, completion: { [weak self] finished in
+                    guard let self = self else { return }
+                    if self.diskView.disk == self._disk {
+                        completion?(finished)
+                    }
+                    guard let diskAfter = self._disk else {
+                        completion?(finished)
+                        return
+                    }
+                    self.diskView.disk = diskAfter
+                    UIView.animate(withDuration: animationDuration / 2, animations: { [weak self] in
+                        self?.layoutDiskView()
+                    }, completion: { finished in
+                        completion?(finished)
+                    })
+                })
+            }
+        } else {
+            if let diskAfter = diskAfter {
+                diskView.disk = diskAfter
+            }
+            completion?(true)
+            setNeedsLayout()
+        }
+    }
+    
+    public func addTarget(_ target: Any?, action: Selector, for controlEvents: UIControl.Event) {
+        button.addTarget(target, action: action, for: controlEvents)
+    }
+    
+    public func removeTarget(_ target: Any?, action: Selector?, for controlEvents: UIControl.Event) {
+        button.removeTarget(target, action: action, for: controlEvents)
+    }
+    
+    public func actions(forTarget target: Any?, forControlEvent controlEvent: UIControl.Event) -> [String]? {
+        button.actions(forTarget: target, forControlEvent: controlEvent)
+    }
+    
+    public var allTargets: Set<AnyHashable> {
+        button.allTargets
+    }
+    
+    public var allControlEvents: UIControl.Event {
+        button.allControlEvents
+    }
+}
